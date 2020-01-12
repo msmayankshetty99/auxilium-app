6c3d020ba351ff9670ae7e6bbd66973a3761384c
Mayank Shetty <msmayankshetty99@gmail.com>
2020-01-12 14:26:35 +0530

    Added Splash Screen

---
+++ app/src/main/java/com/example/auxilium/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.auxilium;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
